c2eb7a9476c82f1fa372a4bfbc4268bebee303e6
chudy.fernandez <chudy.fernandez@72a81004-555f-11de-9272-49eb38e5b1ca>
2010-02-17 05:50:25 +0000

    mrtg final

--- trunk/script/mrtg.sh
+++ trunk/script/mrtg.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 pkg_add -r mrtg
 fetch http://pfsense-cacheboy.googlecode.com/svn/trunk/conf/mrtg.cfg
+fetch http://pfsense-cacheboy.googlecode.com/svn/trunk/script/mrtg_daemon.sh
+mv mrtg_daemon.sh /usr/local/etc/rc.d
+chmod a+x /usr/local/etc/rc.d/mrtg_daemon.sh
+chmod a+rw /var
 tae () {
 sed 's+\(^Workdir: \).*+\1'$dir'+' mrtg.cfg > mrtg.cfg.tmp
 break
 }
 while :
 do
-echo "Mrtg Working Directory path"
-echo "/usr/local/www/mrtg"
+echo "Mrtg Working Directory path (default)"
+echo "/usr/local/mrtg"
 read dir
 case $dir in
 	[/a-zA-Z0-9-]*)
@@ -16,7 +20,15 @@ case $dir in
 	;;
 esac
 done
+ln -s "$dir" /usr/local/www/mrtg
 mv mrtg.cfg.tmp /usr/local/etc/mrtg/mrtg.cfg
 chown mrtg:mrtg /usr/local/etc/mrtg/mrtg.cfg
-mrtg /usr/local/etc/mrtg/mrtg.cfg
-
+cat mrtg.cfg | grep Workdir
+mkdir "$dir"
+chmod a+rw "$dir"
+indexmaker --output="$dir/index.html" /usr/local/etc/mrtg/mrtg.cfg
+/usr/local/etc/rc.d/mrtg_daemon.sh stop
+/usr/local/etc/rc.d/mrtg_daemon stop
+/usr/local/etc/rc.d/mrtg_daemon.sh start
+hostname=`hostname | sed 's/\...*//'`
+echo "You may access your mrtg at http://$hostname/mrtg"
